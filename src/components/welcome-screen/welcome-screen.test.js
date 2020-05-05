d6a5281c81876d0c0ece3aa31c41e61fe9641409
Damir92 <home_damir92@mail.ru>
2020-05-05 22:58:33 +0300

    Add e2e test

--- src/components/welcome-screen/welcome-screen.test.js
+++ src/components/welcome-screen/welcome-screen.test.js
@@ -6,6 +6,7 @@ it(`Render WelcomeScreen`, () => {
   const tree = renderer
     .create(<WelcomeScreen
       errorCount={3}
+      onWelcomeButtonClick={() => {}}
       time={5}
     />)
     .toJSON();
